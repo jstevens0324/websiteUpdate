8203bd0c6e32d2f15e21f8904eabf7d56d7d7235
Josh Stevens <joshua.stevens@avimark.net>
2014-01-20 15:25:50 -0600

    update - created 2 classes and changed work flow

--- index.php
+++ index.php
@@ -6,38 +6,10 @@
  * Time: 1:53 PM
  */
 
-if (isset($_POST['domain']))
-{
-    $file = '../../includes/vhosts.conf';
-    $url = $_REQUEST['domain'];
-    $text = "\n\n#" . strtoupper($url) . "\n" .
-        "<VirtualHost *:80>\n" .
-        "\tServerName www.beth2.petwise.me\n" .
-        "\tServerAlias " . $url . " *." . $url . "\n" .
-        "\tDocumentRoot /home/vetlogic/www/api/public\n" .
-        "\tServerAdmin webmaster@petwisewebsites.com\n\n" .
-        "\tSetEnv APPLICATION_ENV production\n" .
-        "\tSetEnv VETLOGIC_LIBRARY_PATH /home/vetlogic/lib/vetlogic\n\n" .
-        "\t<IfModule mod_suphp.c>\n" .
-        "\t\tsuPHP_UserGroup vetlogic vetlogic\n" .
-        "\t</IfModule>\n" .
-        "\t<IfModule !mod_disable_suexec.c>\n" .
-        "\t\tSuexecUserGroup vetlogic vetlogic\n" .
-        "\t</IfModule>\n" .
-        "</VirtualHost>";
-
-    $fh = fopen($file, 'a+') or die("Can't open file for writing.");
-    fwrite($fh, $text);
-    fclose($fh);
-    echo "Content saved.";
-}
-else
-{
-    ?>
+?>
     <h1>Please enter the domain name</h1>
-    <form action="index.php" method="post">
+    <form action="post" method="get">
         Domain: <input type="text" name="domain"> <br/>
         <input type="submit" value="Submit"/>
     </form>
 <?
-}
---
+++ post.php
@@ -0,0 +1,48 @@
+<?php
+
+
+function __autoload($class_name)
+{
+    include './include/class.' . $class_name . '.inc';
+}
+
+$domains = new Domains();
+//$devFile = '../../includes/vhosts.conf';
+
+
+if (isset($_REQUEST['domain']))
+{
+    //$file = $devFile;
+    $file = '/home/vetlogic/includes/vhosts.conf';
+    $url = $_REQUEST['domain'];
+    if (!$domains->search($file, $url))
+    {
+        $text = "\n\n#" . strtoupper($url) . "\n" .
+            "<VirtualHost *:80>\n" .
+            "\tServerName www.beth2.petwise.me\n" .
+            "\tServerAlias " . $url . " *." . $url . "\n" .
+            "\tDocumentRoot /home/vetlogic/www/api/public\n" .
+            "\tServerAdmin webmaster@petwisewebsites.com\n\n" .
+            "\tSetEnv APPLICATION_ENV production\n" .
+            "\tSetEnv VETLOGIC_LIBRARY_PATH /home/vetlogic/lib/vetlogic\n\n" .
+            "\t<IfModule mod_suphp.c>\n" .
+            "\t\tsuPHP_UserGroup vetlogic vetlogic\n" .
+            "\t</IfModule>\n" .
+            "\t<IfModule !mod_disable_suexec.c>\n" .
+            "\t\tSuexecUserGroup vetlogic vetlogic\n" .
+            "\t</IfModule>\n" .
+            "</VirtualHost>";
+
+        $fh = fopen($file, 'a+') or die("Can't open file for writing.");
+        fwrite($fh, $text);
+        fclose($fh);
+        echo "<h2>Content Saved</h2>";
+    }
+    else
+    {
+        echo "<h2>This domain already exists</h2>";
+
+    }
+    ?><meta http-equiv="refresh" content="2; url=index"><?
+
+}
